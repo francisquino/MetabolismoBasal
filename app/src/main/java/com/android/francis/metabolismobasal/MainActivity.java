45387d0a63eb57eed90a996e2f8a653e59f67285
francisquino <francisjmendez@yahoo.es>
2015-04-20 00:53:31 +0200

    Intento obtener el valor del Spinner peso.

--- app/src/main/java/com/android/francis/metabolismobasal/MainActivity.java
+++ app/src/main/java/com/android/francis/metabolismobasal/MainActivity.java
@@ -3,27 +3,45 @@ package com.android.francis.metabolismobasal;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static android.view.View.OnClickListener;
 
-public class MainActivity extends ActionBarActivity {
+
+public class MainActivity extends ActionBarActivity implements OnClickListener {
 
     private Spinner spinner1;
     private Spinner spinner2;
     private Spinner spinner3;
+
     private List<String> lista;
     private List<String> lista2;
     private List<String> lista3;
+
+
+    TextView mainTextView;
+    Button mainButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //1. Rellenar los datos por defecto de los spinners.
         datosPorDefecto();
+
+        // 2. Access the Button defined in layout XML
+        // and listen for it here
+        mainButton = (Button) findViewById(R.id.main_button);
+        mainButton.setOnClickListener(this);
+
     }
 
     private void datosPorDefecto() {
@@ -71,6 +89,28 @@ public class MainActivity extends ActionBarActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    @Override
+    public void onClick(View v) {
+        int calorias=0;
+
+        // Obtener los valores de Sexo, Peso, Altura y Edad y realizar el cálculo.
+        // Escribir el valor en mainTextView
+
+        // Obtener sexo
+        String sexo = "Hombre";
+
+        // Obtener peso
+        Spinner mySpinner=(Spinner) findViewById(R.id.spinnerPeso);
+        int peso =  Integer.valueOf((String) mySpinner.getSelectedItem());
+
+        // Cálculo de las calorias.
+        if (sexo=="Hombre") {
+            calorias = peso;
+        }
+
+        mainTextView.setText(calorias);
     }
 
 }
